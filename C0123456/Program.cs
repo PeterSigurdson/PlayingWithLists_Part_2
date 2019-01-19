9d91e0d169fc233252818c541512e816a6410dc3
visualstudio <visualstudio@122-INSTRUCTOR>
2019-01-18 20:40:53 -0500

    added the code for the StudentInformatin object

--- C0123456/Program.cs
+++ C0123456/Program.cs
@@ -10,6 +10,25 @@ namespace C0123456
     {
         static void Main(string[] args)
         {
+            StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 2.0, 10);
+            StudentInformation _Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation _Steve = new StudentInformation("Steve", 2.9, 16);
+            StudentInformation _Cindy = new StudentInformation("Mary", 3.9, 187);
         }
+    }
+
+    class StudentInformation
+    {
+        public string Name;
+        public double GPA;
+        public int Attendance;
+        public StudentInformation(String  aName, double cGPA, int dAttendence)
+        {
+            Name = aName;
+            GPA = cGPA;
+            Attendance = dAttendence;
+        }
+           
     }
 }
